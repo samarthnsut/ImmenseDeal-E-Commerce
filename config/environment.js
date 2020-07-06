be718fdbf13461c6b79c599ad4d34400d469e79a
samarth <45564142+samarthnsut@users.noreply.github.com>
2020-07-07 01:34:26 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -2,23 +2,10 @@
 const development= {
     name: "development",
     cookie: "secret123",
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     db:"mongodb+srv://Samarth:samarth@cluster0-xf5s7.mongodb.net/<database name>?retryWrites=true&w=majority",
-=======
-    db:"mongodb+srv://Samarth:samarth@cluster0-xf5s7.mongodb.net/<dbname>?retryWrites=true&w=majority",
->>>>>>> parent of 1e90f6d... Update environment.js
-=======
+
     db:"mongodb+srv://Samarth:samarth@cluster0-xf5s7.mongodb.net/<database name>?retryWrites=true&w=majority",
->>>>>>> parent of 83ed284... Update environment.js
-=======
-    db:"mongodb+srv://Samarth:samarth@cluster0-xf5s7.mongodb.net/<database name>?retryWrites=true&w=majority",
->>>>>>> parent of 83ed284... Update environment.js
-=======
-    db:"mongodb+srv://Samarth:samarth@cluster0-xf5s7.mongodb.net/<database name>?retryWrites=true&w=majority",
->>>>>>> parent of 83ed284... Update environment.js
+
     smtp:    {
         service : 'gmail',
         host: "smtp.gmail.com",
